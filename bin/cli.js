c0e181ecbcd978f32ad9313574b03daf90dad7f3
Alejandro Lembke Barrientos <aleleba@hotmail.com>
2022-04-29 14:31:14 +0000

    Start up of the project.

---
+++ bin/cli.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+const { execSync } = require('child_process');
+
+const runCommand = command => {
+    try{
+        execSync(`${command}`, {stdio: 'inherit'});
+    } catch (e) {
+        console.error(`Failed to execute ${command}`, e);
+        return false;
+    }
+    return true;
+}
+
+const repoName = process.argv[2];
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/aleleba/create-react-ssr ${repoName}`;
+const installDepsCommand = `cd ${repoName} && npm install`;
+
+console.log(`Cloning the repository with name ${repoName}`);
+const checkedOut = runCommand(gitCheckoutCommand);
+if(!checkedOut) process.exit(-1);
+
+console.log(`Installing dependencies for ${repoName}`);
+const installedDeps = runCommand(installDepsCommand);
+if(!installedDeps) process.exit(-1);
+
+console.log("Congratulations! You are ready. Follow the following commands to start");
+console.log(`cd ${repoName}`);
+console.log('Create a .env file with ENV=development(defauld: production), PORT=3000 (default: 80), PUBLIC_URL=your_public_url(optional)(default: /)');
+console.log(`Then you can run: npm start:dev`);
