25d436dd84fe18e0687ed0728dacf91426358136
Alejandro Lembke Barrientos <aleleba@hotmail.com>
2022-07-29 04:00:04 +0000

    PR-091792: Adding support for windows.

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 const { execSync } = require('child_process');
+const isWin = process.platform === "win32";
 
 const runCommand = command => {
     try{
@@ -14,18 +15,20 @@ const runCommand = command => {
 const repoName = process.argv[2];
 const gitCheckoutCommand = `git clone --depth 1 https://github.com/aleleba/create-react-ssr ${repoName}`;
 const installDepsCommand = `cd ${repoName} && rm -rf .git && git init && git add . && git commit -m "Initial commit" && npm install`;
+const installDepsCommandWindows = `cd ${repoName} && rd -r .git && git init && git add . && git commit -m "Initial commit" && npm install`;
 const deleteFoldersCommand = `cd ${repoName} && rm -rf .github &&  rm -rf bin`
+const deleteFoldersCommandWindows = `cd ${repoName} && rd -r .github &&  rd -r bin`
 
 console.log(`Cloning the repository with name ${repoName}`);
 const checkedOut = runCommand(gitCheckoutCommand);
 if(!checkedOut) process.exit(-1);
 
 console.log(`Installing dependencies for ${repoName}`);
-const installedDeps = runCommand(installDepsCommand);
+const installedDeps = isWin ? runCommand(installDepsCommandWindows) : runCommand(installDepsCommand);
 if(!installedDeps) process.exit(-1);
 
 console.log(`Delete folders for ${repoName}`);
-const deleteFolders = runCommand(deleteFoldersCommand);
+const deleteFolders = isWin ? runCommand(deleteFoldersCommandWindows) : runCommand(deleteFoldersCommand);
 if(!deleteFolders) process.exit(-1);
 
 console.log("Congratulations! You are ready. Follow the following commands to start");
